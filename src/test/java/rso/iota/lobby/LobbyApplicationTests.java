c6484eecdd833e7b6b9c7cbd26dda765208aab4a
Jan Vasiljevic <vasiljevic.jan@gmail.com>
2024-11-19 14:01:08 +0100

    Remove tests to test CI

--- src/test/java/rso/iota/lobby/LobbyApplicationTests.java
+++ src/test/java/rso/iota/lobby/LobbyApplicationTests.java
@@ -1,13 +1,12 @@
 package rso.iota.lobby;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LobbyApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
